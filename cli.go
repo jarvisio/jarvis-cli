46f072b3ec07c68f2d17180e6e65e2e62c2ea236
Piet Brauer <piet@nerdishbynature.com>
2014-11-11 20:50:32 +0100

    Add time command

--- cli.go
+++ cli.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"github.com/codegangsta/cli"
+	"github.com/jarvisio/commands"
 	"os"
+	"os/exec"
+	"runtime"
 )
 
 func main() {
@@ -10,5 +13,30 @@ func main() {
 	app.Version = "0.0.1"
 	app.Name = "jarvis"
 	app.Usage = "I am a nice butler, who can speak and listen"
+	app.Commands = []cli.Command{timeCommand()}
 	app.Run(os.Args)
+}
+
+func timeCommand() cli.Command {
+	command := cli.Command{
+		Name:      "clock",
+		ShortName: "c",
+		Usage:     "Tells you the current time",
+		Action: func(c *cli.Context) {
+			say("The time is" + commands.Now(false))
+		},
+	}
+	return command
+}
+
+func say(text string) {
+	os := runtime.GOOS
+	switch os {
+	case "linux":
+		exec.Command("tts", text).Run()
+	case "darwin":
+		exec.Command("say", text).Run()
+	default:
+		println(text)
+	}
 }
